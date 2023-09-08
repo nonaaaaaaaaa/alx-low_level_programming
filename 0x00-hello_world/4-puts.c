7c02fde734f39494cc6162aed300b9a748db6d10
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-09-07 17:14:49 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - "main is entry point"
+* 'main' - "main is entry point"
 * Description - "A c  program how to use puts function"
 * Return - "always 0(success)"
 */
