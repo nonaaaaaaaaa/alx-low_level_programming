c75313b05e2be5a86a95a9749704ccef2a0018ae
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-09-14 10:07:49 -0700

    l

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - function print straight line
+ * @n: variable
+ * Return: no return
+ */
+
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
