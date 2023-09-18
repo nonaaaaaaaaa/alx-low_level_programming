b45a6182849ce28760087f600a4538cf83a6c4cb
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-09-18 08:46:51 -0700

    l

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - function to print string
+ * @str: input string
+ *
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + '0');
+		str++;
+	}
+	_putchar('\n');
+}
