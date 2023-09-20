b68eda4fa988e4d6886d16d4696d1574b039e6ec
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-09-20 08:51:09 -0700

    l

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,19 +6,15 @@
  *  Return: dest
  *
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int destlen = 0;
-	int srclen = 0;
-	int i;
+	int d, s;
 
-	for (i = 0; dest[i] != '0'; i++)
-		destlen++;
-	for (i = 0; src[i] != '0'; i++)
-		srclen++;
-	for (i = 0; i <= srclen; i++)
-		dest[destlen + i] = src[i];
+	d = 0;
+	while (dest[d])
+		d++;
+	for (s = 0; src[s]; s++)
+		dest[d++] = src[s];
 
 	return (dest);
 }
