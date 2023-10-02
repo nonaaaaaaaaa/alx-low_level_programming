2f4e69de6346d229b5813c9ebc826107db1d845a
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-10-02 12:48:40 -0700

    l

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,18 +8,8 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
-
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-		{
-			printf("%d\n", i - 1);
-		}
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
