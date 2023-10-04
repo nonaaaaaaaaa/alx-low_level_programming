0255e2922680e3ed0e330096fb551f2c53c9e37d
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-10-04 08:15:08 -0700

    l

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,22 +11,24 @@
 
 char *_strdup(char *str)
 {
-	int i = 0, size = 0;
-	char *m;
+	 unsigned int i;
+	 int size;
+	char *m = NULL;
 
 	if (str == NULL)
 		return (NULL);
-
-	for (; str[size] != '\0'; size++)
+	for (size = 0; str[size] != '\0'; size++)
 		;
-	m = malloc(size * sizeof(*str) + 1);
-
-	if (m == 0)
-		return (NULL);
-
-
-		for (; i < size; i++)
+	m = (char *)malloc(size + 1 * sizeof(char));
+	if (m != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
 			m[i] = str[i];
-
+	}
+	else
+	{
+		return (NULL);
+	}
+	m[i] = '\0';
 	return (m);
 }
