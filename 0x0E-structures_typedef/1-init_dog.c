ccfc7142396a14a1edea96dcd2e57823a06a735c
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-10-10 10:44:59 -0700

    l

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - function that initialize a variable
@@ -12,10 +13,12 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d == NULL)
 	{
+		return;
+	}
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-	}
+
 }
