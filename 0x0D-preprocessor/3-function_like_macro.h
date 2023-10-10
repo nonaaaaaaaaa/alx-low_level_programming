830b651558d68c77a932d5da334fe2aa5acc76fb
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-10-10 08:24:03 -0700

    l

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
+
+/**
+ * File: 3-function_like_macro.h
+ * @x: input
+ * ABS(x) - function computes the absolute value of a number x
+ * Desc: header file
+ *
+ */
+
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+
+#endif /* _FUNCTION_LIKE_MACRO_H */
