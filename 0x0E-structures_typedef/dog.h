bf13d51bf8d09523093fafc7fd1b310ca3d6d2a4
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-10-10 09:54:53 -0700

    l

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * File: dog.h
+ * struct dog - struct new type
+ * @age: age dog
+ * @owner : dog owner
+ * @name: dog name
+ * Desc: header file
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
