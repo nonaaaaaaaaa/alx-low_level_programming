525dccdb36e2a2bde2517159c328a3802263bc75
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-10-10 11:51:09 -0700

    l

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function to free mem
+ * @d: pointer to dog_t
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
