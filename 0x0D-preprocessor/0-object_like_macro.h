eaa6d3b1b7d0e35f0555a96a5756600928d3c863
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-10-10 07:35:44 -0700

    l

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef _OBJECT_LIKE_MACRO_H
+#define _OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro.h
+ *Desc: header file
+ *
+ */
+#define SIZE 1024
+
+#endif  /* _OBJECT_LIKE_MACRO_H */
