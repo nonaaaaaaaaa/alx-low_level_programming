74eb6ecd06ca2679488d559cf2b0aa681de44773
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-10-10 08:33:16 -0700

    l

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef _SUM_H
+#define _SUM_H
+
+/**
+ * File : 4-sum.h
+ * SUM(x, y) - function computes the sum of two numbers
+ * @x: input
+ * @y: input
+ * Desc: header file
+ *
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /* _SUM_H */
