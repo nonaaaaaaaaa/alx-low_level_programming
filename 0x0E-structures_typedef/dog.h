2301d14664c4a76d64a7c0ce83914f14a5ec1cae
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-10-11 06:46:02 -0700

    l

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
