48e3c3a0e7608807d4745fc7841795601b6f1ab5
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-10-23 11:02:05 -0700

    l

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list
+ * @head: apointer to address of list
+ * Description: sets the head to NULL
+ * Return: nothing
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+
+	head = NULL;
+}
